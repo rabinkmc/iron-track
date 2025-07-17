32bc4242bc626ca522ee274b3eedb250a80e30d5
Rabin Dhamala <rabinkmc@gmail.com>
2025-07-17 15:47:37 +0545

    use simplejwt

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "api",
     "rest_framework",
     "django_extensions",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
