2fda8f592451cff85804a7383f471ae684e621bf
Rabin Dhamala <rabinkmc@gmail.com>
2025-07-22 23:13:51 +0545

    add workout session exercise set in admin

--- backend/iron/admin.py
+++ backend/iron/admin.py
@@ -4,8 +4,10 @@ from iron.models import (
     Exercise,
     WorkoutSession,
     WorkoutSessionExercise,
+    WorkoutSessionExerciseSet,
 )
 
 admin.site.register(Exercise)
 admin.site.register(WorkoutSession)
 admin.site.register(WorkoutSessionExercise)
+admin.site.register(WorkoutSessionExerciseSet)
