f2dfb980d4914ff3f82e52b5a607292293dedb82
Rabin Dhamala <rabinkmc@gmail.com>
2025-07-23 10:41:03 +0545

    remove home page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.http.response import HttpResponse
 from django.urls import path, include
 
 
-def home(request):
-    return HttpResponse("Welcome to the Home Page")
-
-
 urlpatterns = [
-    path("", home, name="home"),
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
